87ca4a7d63d521d88d09236a53e79fe227bda037
Maximiliano Arce <centauroia39@gmail.com>
2026-01-28 15:00:30 -0300

    creacion de bucle for con parametros especificos

--- wordle.py
+++ wordle.py
@@ -4,4 +4,13 @@ letras_verificadas = []
 cantidad_letras = 5
 # cree una funcion que me ayude a comparar la palabra ingresada con la plabra secreta
 def verificador_palabra(palabra_ingresada, palabra_secreta):
-    
+# creo n bucle for con los parametros necesarios
+    for i in range(cantidad_letras):
+        las_palabras_son_iguales = palabra_ingresada[i] == palabra_secreta[i] # True o False
+        la_letra_existe_en_la_palabra = palabra_ingresada[i] in palabra_secreta 
+        
+        if las_palabras_son_iguales:
+            letras_verificadas.append(f"[{palabra_ingresada[i]}]")
+        elif la_letra_existe_en_la_palabra:
+            letras_verificadas.append(f"({palabra_ingresada[i]})")
+        else:
