99c9747c00408e13e967d327826a7e788500b8c4
Maximiliano Arce <centauroia39@gmail.com>
2026-01-28 15:05:29 -0300

    creacion de bucle while para definir cantidad de intentos

--- wordle.py
+++ wordle.py
@@ -14,3 +14,16 @@ def verificador_palabra(palabra_ingresada, palabra_secreta):
         elif la_letra_existe_en_la_palabra:
             letras_verificadas.append(f"({palabra_ingresada[i]})")
         else:
+         letras_verificadas.append(palabra_ingresada[i])
+    
+    return letras_verificadas
+
+verificador_palabra('holas', 'calor')
+#creo un bucle while para definir cantidad de intentos
+intentos = 0
+
+while intentos < 6:
+    print(f"te quedan {6 - intentos} intentos")
+    intentos = intentos + 1
+    palabra_ingresada = input("Ingrese una palabra")
+    print(f"la palabra ingresada es: {palabra_ingresada}")
