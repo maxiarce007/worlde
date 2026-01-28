2cd496e00a734dae1d5bf0c4ee4679601e09acbf
Maximiliano Arce <centauroia39@gmail.com>
2026-01-28 14:56:16 -0300

    creacion de funciones comparativas

--- wordle.py
+++ wordle.py
@@ -2,3 +2,6 @@
 letras_verificadas = []
 #luego defino la cantidad de letras permitidas
 cantidad_letras = 5
+# cree una funcion que me ayude a comparar la palabra ingresada con la plabra secreta
+def verificador_palabra(palabra_ingresada, palabra_secreta):
+    
